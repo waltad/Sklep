da4c21379a1a1c4a6dcdbafb05d01078fc06998c
Tadek <tadek.waluga@gmail.com>
2021-09-26 10:46:52 +0200

    Pierwsza przymiarka do modeli w pliku models

---
+++ shop/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.db.models import (Model,
+                              CharField, DateField, DateTimeField, IntegerField, TextField,
+                              ForeignKey, DO_NOTHING, )
+# Create your models here.
+
+class Product(Model):
+    category = CharField(max_length=128)
+    photo = CharField(max_length=128)
+    description = TextField
+    amount = IntegerField
+    price = IntegerField
+
+
+class Client(Model):
+    name = CharField(max_length=128)
+    surname = CharField(max_length=128)
+    street = CharField(max_length=128)
+    home_number = CharField(max_length=128)
+    flat_number = CharField(max_length=128)
+    postcode = CharField(max_length=128)
+    locality = CharField(max_length=128)
+    country = CharField(max_length=128)
+    mail = CharField(max_length=128)
+    phone_number = CharField(max_length=128)
+
+
+class Delivery(Model):
+    delivery_company = CharField(max_length=128)
+    delivery_price = IntegerField
+
+
+class Basket(Model):
+    product = ForeignKey(Product)
+    amount = IntegerField
+    client = ForeignKey(Client)
+
+
+class Order(Model):
+    delivery = ForeignKey(Delivery)
+    client = ForeignKey(Client)
+    date_order = DateTimeField(auto_created=True)
+
+
+class Product_in_order(Model):
+    product =
+    order = ForeignKey(Order)
+    product = ForeignKey(Product)
+    amount = IntegerField
+
+
+
+
+
+
+
+
